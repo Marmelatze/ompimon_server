05d05ab6696d9b520f502bd0b1c60548255e827a
Florian Pfitzer <pfitzer@w3p.cc>
2013-03-27 09:10:11 +0100

    ignore

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 var net = require('net');
 
 var port = 8214;
-var server = net.createServer(function(c) { //'connection' listener
-  console.log('server connected');
-  c.on('end', function() {
-    console.log('server disconnected');
-  });
-  c.write('hello\r\n');
-  c.pipe(c);
+var server = net.createServer(function (c) { //'connection' listener
+    console.log('server connected');
+    c.on('end', function () {
+        console.log('server disconnected');
+    });
+    c.write('hello\r\n');
+    c.pipe(c);
 });
-server.listen(port, function() { //'listening' listener
-  console.log("Server started on port " + port);
+
+
+server.listen(port, function () { //'listening' listener
+    console.log("Server started on port " + port);
 });
