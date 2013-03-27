6751d4e16860de1b309cc222f9d7b71a37c1f38c
Florian Pfitzer <pfitzer@w3p.cc>
2013-03-27 08:40:01 +0100

    simple server

---
+++ server.js
@@ -0,0 +1,14 @@
+var net = require('net');
+
+var port = 8214;
+var server = net.createServer(function(c) { //'connection' listener
+  console.log('server connected');
+  c.on('end', function() {
+    console.log('server disconnected');
+  });
+  c.write('hello\r\n');
+  c.pipe(c);
+});
+server.listen(port, function() { //'listening' listener
+  console.log("Server started on port " + port);
+});
