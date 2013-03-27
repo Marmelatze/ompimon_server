babbd447b2a3199499ed48a5300684db74430870
Florian Pfitzer <pfitzer@w3p.cc>
2013-03-27 10:03:10 +0100

    server cluster

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 -Testing the pushing to the repo
+
+
+## Node.js Plugin für Phpstorm ##
+
+Anleitung: http://blog.jetbrains.com/webide/2012/03/attaching-the-sources-of-node-js-core-modules/
+
--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 var net = require('net');
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 
-var port = 8214;
-var server = net.createServer(function (c) { //'connection' listener
-    console.log('server connected');
-    c.on('end', function () {
-        console.log('server disconnected');
+if (cluster.isMaster) {
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+        console.log("forked");
+    }
+    cluster.on('exit', function (worker, code, signal) {
+        console.log('worker ' + worker.process.pid + ' died');
     });
-    c.write('hello\r\n');
-    c.pipe(c);
-});
+} else {
+    startServer();
+}
 
 
-server.listen(port, function () { //'listening' listener
-    console.log("Server started on port " + port);
-});
+function startServer() {
+    var port = 8214;
+    var server = net.createServer(function (c) { //'connection' listener
+        console.log('server connected');
+        c.on('end', function () {
+            console.log('server disconnected');
+        });
+        c.write('hello\r\n');
+        c.pipe(c);
+    });
+
+
+    server.listen(port, function () { //'listening' listener
+        console.log("Server started on port " + port);
+    });
+
+}
