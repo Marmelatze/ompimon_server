0c0bcfedeb15bfe60ecf8a36690cecd327e8fde5
Lukas Schmitt <schmitt@w3p.cc>
2013-04-03 10:37:48 +0200

    test authentification for cluster #22

---
+++ clusterTest/app.js
@@ -0,0 +1,42 @@
+/**
+ * @author Lukas Schmitt <schmitt@w3p.cc>
+ * Date: 03.04.13
+ * Time: 09:28
+ *
+ */
+var net = require('net');
+var client = net.connect({port: 8214}, function() { //'connect' listener
+        console.log('client connected');
+
+        auth();
+
+        /*buf = new Buffer(9);
+        len = buf.write('1 + 2 = 3', 0);
+        console.log(len + " bytes: " + buf.toString('utf8', 0, len));
+        client.write(buf);*/
+    });
+client.on('data', function(data) {
+    console.log(data.toString('utf8'));
+    client.end();
+});
+client.on('end', function() {
+    console.log('client disconnected');
+});
+
+function auth(){
+    var username = 'test';
+    var password = 'install';
+    var buf = new Buffer(256);
+    var len = 0;
+    buf.writeUInt8('0', 0);
+    len++;
+    buf.writeUInt8(username.length, len);
+    len++;
+    len += buf.write(username, len);
+    buf.writeUInt8(password.length, len);
+    len++;
+    len += buf.write(password, len);
+
+    finBuf = buf.slice(0,len);
+    client.write(finBuf);
+}
