21e7e14ff35c91fdb59d901312bcb0df31ff21af
Florian Pfitzer <pfitzer@w3p.cc>
2013-04-05 18:12:30 +0200

    added binary syntax and underscore js

---
+++ cluster/protocol/actions/action.js
@@ -0,0 +1,18 @@
+var eventEmitter = require('events').EventEmitter
+;
+
+
+exports.Action = Action;
+
+function Action () {
+    eventEmitter.call(this);
+}
+
+// inherit events.EventEmitter
+Action.super_ = eventEmitter;
+Action.prototype = Object.create(eventEmitter.prototype, {
+    constructor: {
+        value: Action,
+        enumerable: false
+    }
+});
--- cluster/worker.js
+++ cluster/worker.js
@@ -1,7 +1,10 @@
 var eventEmitter = require('events').EventEmitter,
     net = require('net'),
     client = require('./client'),
-    auth = require('./auth').auth
+    protocol = require('./protocol'),
+    crypto = require("crypto"),
+    BufferBuilder = require('buffer-builder')
+
 ;
 
 
@@ -29,9 +32,10 @@ Worker.prototype.start = function() {
     var self = this;
     var server = net.createServer(function (c) { //'connection' listener
         var clientInstance = new client.client(c);
+
         self.connect(clientInstance);
         c.on('end', function () {
-            console.log('server disconnected');
+            console.log('client '+clientInstance.id+' disconnected');
         });
 
     });
@@ -48,7 +52,11 @@ Worker.prototype.start = function() {
  */
 Worker.prototype.connect = function(client) {
     var self = this;
-    client.socket.write("Go Away!\r\n");
+    if (!client.id) {
+        client.id = crypto.createHash('md5').update(crypto.randomBytes(512)).digest('hex');
+    }
+    console.log("client "+ client.id+" connected");
+    client.socket.write("Welcome!\r\n");
     client.socket.on('data', function(data) {
         self.process(client, data);
     });
@@ -60,40 +68,23 @@ Worker.prototype.connect = function(client) {
  * @param data
  */
 Worker.prototype.process = function(client, data) {
-    data = data.toString('utf-8').trim().replace(/(\r\n|\n|\r)/gm,"");
-    console.log("received:" + data);
-
-    var socket = client.socket;
-
-    var args = data.split(" ");
-    if (args.length < 0 ) {
-        socket.write("Invalid input\r\n");
-    }
-
-    if ("quit" == args[0]) {
-        socket.write("goodbye\r\n");
-        socket.end();
-
-        return;
-    }
-    if ("auth" == args[0]) {
-
-        var username = args[1];
-        var password = args[2];
-        auth.authenticate(username, password, function() {
-            client.authenticated = true;
-            socket.write("OK\r\n");
-        }, function() {
-            client.authenticated = false;
-            socket.write("FAILED\r\n");
+    var string = data.toString('utf-8').trim().replace(/(\r\n|\n|\r)/gm,"");
+    console.log("received from " + client.id+":");
+    console.log(data);
+    console.log(string);
+    try {
+        protocol.parse(client, data, function(buf) {
+            if (null !== buf) {
+                client.socket.write(buf);
+            }
         });
-
-        return;
+    } catch (e) {
+        console.log(e);
+        var result = new BufferBuilder();
+        result.appendUInt8(0x02); // invalid input
+        client.socket.write(result.get());
     }
 
-    if (!client.authenticated) {
-        socket.write("not authenticated\r\n");
-    }
 
 
 };
