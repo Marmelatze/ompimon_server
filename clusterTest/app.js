61cfb06f3bf253022b50a8de14766c083819b95a
Lukas Schmitt <schmitt@w3p.cc>
2013-04-07 15:27:51 +0200

    cluster simuator #22

--- clusterTest/app.js
+++ clusterTest/app.js
@@ -6,42 +6,48 @@
  */
 var net = require('net');
 var BufferBuilder = require('buffer-builder');
+var binary = require('binary');
 var program = require('commander');
 
-var client = net.connect({port: 8214}, function() { //'connect' listener
-    //var username = 'test';
-    //var password = 'install';
+var client = net.connect({port: 8214}, function() {
     console.log('client connected');
 
-    program.prompt('Enter username: ', function(username){
-        console.log('Your username is %s', username);
-
-        program.password('Enter password: ', '*', function(password){
-            process.stdin.destroy();
-            auth(username, password);
-        });
-
-    });
+    auth();
 });
 
 client.on('data', function(data) {
-    console.log("received:");
-    console.log(data);
+    console.log("received:", data);
     console.log(data.toString('utf8'));
+
+    var parseData = binary.parse(data).word8lu('type').vars;
+
+    console.dir(parseData);
+
 });
 client.on('end', function() {
     console.log('client disconnected');
 
 });
 
-function auth(username, password){
+function auth(){
+    client.pause();
+    program.prompt('Enter username: ', function(username){
+        console.log('Your username is %s', username);
 
-    var auth = new BufferBuilder();
-    auth.appendUInt8(0);
-    auth.appendUInt8(username.length);
-    auth.appendString(username);
-    auth.appendUInt8(password.length);
-    auth.appendString(password);
+        program.password('Enter password: ', '*', function(password){
+            process.stdin.destroy();
+            var auth = new BufferBuilder();
+            auth.appendUInt8(0);
+            auth.appendUInt8(username.length);
+            auth.appendString(username);
+            auth.appendUInt8(password.length);
+            auth.appendString(password);
 
-    client.write(auth.get());
+            client.write(auth.get());
+            client.resume();
+        });
+
+    });
+
+
 }
