d8fabcabc52856a422e9f9f3841a919738205821
Florian Pfitzer <pfitzer@w3p.cc>
2013-04-09 11:02:34 +0200

    added unit tests

--- cluster/auth.js
+++ cluster/auth.js
@@ -3,10 +3,14 @@ var eventEmitter = require('events').EventEmitter,
 ;
 
 
-
-
-function Auth() {
+/**
+ * Authentication class
+ * @param type default: http. Can be one of http or test. Http authenticates against http backend.
+ * @constructor
+ */
+function Auth(type) {
     eventEmitter.call(this);
+    this.type = type || 'http';
 }
 
 // inherit events.EventEmitter
@@ -18,7 +22,33 @@ Auth.prototype = Object.create(eventEmitter.prototype, {
     }
 });
 
-Auth.prototype.authenticate = function(user, password, callback, errorCallback) {
+/**
+ * Authenticate a user
+ * @param user
+ * @param password
+ * @param callback will be called if authentication is successfull
+ * @param errorCallback will be called if authentication failed
+ */
+Auth.prototype.authenticate= function(user, password, callback, errorCallback) {
+    switch (this.type) {
+        case 'http':
+            this.authenticateHTTP(user, password, callback, errorCallback);
+            break;
+        case 'test':
+            this.authenticateTest(user, password, callback, errorCallback);
+            break;
+    }
+};
+
+/**
+ * Authenticate a user against the http backend
+ * @param user
+ * @param password
+ * @param callback
+ * @param errorCallback
+ * @returns {*}
+ */
+Auth.prototype.authenticateHTTP = function(user, password, callback, errorCallback) {
     var self = this;
     http.get({
         hostname: "localhost",
@@ -37,6 +67,21 @@ Auth.prototype.authenticate = function(user, password, callback, errorCallback) 
     });
 
     return this;
+};
+
+/**
+ * Dummy authentication function used for unit testing
+ * @param user
+ * @param password
+ * @param callback
+ * @param errorCallback
+ */
+Auth.prototype.authenticateTest = function(user, password, callback, errorCallback) {
+    if (user == "test" && password == "test") {
+        callback();
+    } else {
+        errorCallback();
+    }
 };
 
 exports.auth = new Auth();
---
+++ cluster/test/testAuth.js
@@ -0,0 +1,56 @@
+var protocol = require("../protocol"),
+    auth = require("../auth").auth,
+    BufferBuilder = require("buffer-builder"),
+    binary = require("binary"),
+    client = require("../client.js");
+
+auth.type = "test"; // make authentication testable
+
+var client = new client.client(new Buffer(256));
+client.id = "1234567890yyyasdfasdfasdfasdfasd";
+
+// test successfull auth
+
+exports.testAuthSuccess = function(test) {
+    var buffer = new BufferBuilder();
+    buffer.appendUInt8(0x00);
+    var username = "test";
+    var password = "test";
+    buffer.appendUInt8(username.length);
+    buffer.appendString(username);
+    buffer.appendUInt8(password.length);
+    buffer.appendString(password);
+
+    protocol.parse(client, buffer.get(), function(result) {
+        var vars = binary.parse(result)
+            .word8('result')
+            .buffer('id', 32)
+            .vars
+        ;
+        test.equal(vars.result, 0);
+        test.equal(vars.id.toString('utf8'), client.id);
+        test.done();
+    });
+
+};
+// test failing auth
+exports.testAuthFailure = function(test) {
+    var buffer = new BufferBuilder();
+    buffer.appendUInt8(0x00);
+    var username = "test2";
+    var password = "test2";
+    buffer.appendUInt8(username.length);
+    buffer.appendString(username);
+    buffer.appendUInt8(password.length);
+    buffer.appendString(password);
+
+    protocol.parse(client, buffer.get(), function(result) {
+        var vars = binary.parse(result)
+            .word8('result')
+            .vars
+        ;
+        test.equal(vars.result, 1);
+        test.done();
+    });
+};
+
